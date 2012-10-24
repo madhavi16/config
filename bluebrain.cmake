aa8de88e742ddbb2c118ece73c0c43c1ddd2f5c6
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2012-10-24 14:54:50 +0200

    Moved from Eyescale/config & Eyescale/Buildyard

---
+++ bluebrain.cmake
@@ -0,0 +1,6 @@
+
+set(BLUEBRAIN_REPO_URL https://github.com/BlueBrain/bluebrain.github.com)
+set(BLUEBRAIN_REPO_TAG master)
+set(BLUEBRAIN_NOTEST ON)
+set(BLUEBRAIN_SKIPFIND ON)
+set(BLUEBRAIN_OPTIONAL ON)
