b8ddaa90dde9f1349b5d2dd724c12a15f7e8036d
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2012-11-27 12:03:13 +0100

    Prepare 1.0 release

--- DASH.cmake
+++ DASH.cmake
@@ -1,7 +1,7 @@
 
-set(DASH_PACKAGE_VERSION 0.1.0)
+set(DASH_PACKAGE_VERSION 1.0.0)
 set(DASH_DEPENDS bluebrain REQUIRED Lunchbox Boost)
 set(DASH_BOOST_COMPONENTS serialization)
 set(DASH_REPO_URL https://github.com/BlueBrain/dash.git)
-set(DASH_REPO_TAG master)
+set(DASH_REPO_TAG release-1.0.0)
 set(DASH_SOURCE "${CMAKE_SOURCE_DIR}/src/dash")
