443623814cbf8d6449b6e51ade38e87ae10a24ee
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2012-12-18 16:37:20 +0100

    Another DASH->dash rename

--- codash.cmake
+++ codash.cmake
@@ -1,6 +1,6 @@
 
 set(CODASH_PACKAGE_VERSION 0.1.0)
-set(CODASH_DEPENDS bluebrain REQUIRED DASH Collage Boost)
+set(CODASH_DEPENDS bluebrain REQUIRED dash Collage Boost)
 set(CODASH_REPO_URL https://github.com/BlueBrain/codash.git)
 set(CODASH_REPO_TAG master)
 set(CODASH_BOOST_COMPONENTS "program_options serialization")
