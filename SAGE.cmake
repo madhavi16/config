cdb70932b4acda845a7ed0591899b0234eb579aa
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2013-01-30 10:20:50 +0100

    Fix SAGE-bootstrap

--- SAGE.cmake
+++ SAGE.cmake
@@ -6,14 +6,16 @@ set(SAGE_SOURCE "${CMAKE_SOURCE_DIR}/src/SAGE")
 set(SAGE_NOTEST ON)
 set(SAGE_OPTIONAL ON)
 
-if(EXISTS ${SAGE_SOURCE}/configured)
-  set(_sage_configure_cmd true)
-else()
-  set(_sage_configure_cmd cd ${SAGE_SOURCE} && ./configure)
-endif()
+set(_sage_configure_cmd ${CMAKE_BINARY_DIR}/SAGE/_sage_configure_cmd.cmake)
+file(WRITE ${_sage_configure_cmd}
+  "if(NOT EXISTS ${CMAKE_BINARY_DIR}/SAGE/config.status)\n
+    execute_process(COMMAND ./configure WORKING_DIRECTORY ${SAGE_SOURCE})\n
+    execute_process(COMMAND ${CMAKE_COMMAND} -E touch config.status WORKING_DIRECTORY ${CMAKE_BINARY_DIR}/SAGE)\n
+   endif()\n"
+)
 
 set(SAGE_EXTRA
-  CONFIGURE_COMMAND ${_sage_configure_cmd}
+  CONFIGURE_COMMAND ${CMAKE_COMMAND} -P ${_sage_configure_cmd}
   BUILD_COMMAND cd ${SAGE_SOURCE} && make -f Makefile
   INSTALL_COMMAND cd ${SAGE_SOURCE} && make -f Makefile PREFIX="${CMAKE_CURRENT_BINARY_DIR}/install/sage" install
 )
