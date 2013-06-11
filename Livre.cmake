2ca73559856339435eb9ab849c9cf55208f7f741
Daniel Nachbaur <daniel.nachbaur@epfl.ch>
2013-06-11 18:28:01 +0200

    Add Livre config

---
+++ Livre.cmake
@@ -0,0 +1,6 @@
+
+set(LIVRE_PACKAGE_VERSION 1.0.0)
+set(LIVRE_REPO_URL https://github.com/BlueBrain/Livre.git)
+set(LIVRE_DEPENDS Qt4 REQUIRED Boost Collage Equalizer Lunchbox OpenGL PNG Threads)
+set(LIVRE_BOOST_COMPONENTS "date_time")
+set(LIVRE_QT4_COMPONENTS "QtCore QtGui")
