6e31faca41e7b382d1537d56df6daff19013beb7
Daniel Nachbaur <danielnachbaur@gmail.com>
2013-06-12 21:01:23 +0300

    Update Livre.cmake

--- Livre.cmake
+++ Livre.cmake
@@ -1,7 +1,7 @@
-
 set(LIVRE_PACKAGE_VERSION 1.0.0)
 set(LIVRE_REPO_URL https://github.com/BlueBrain/Livre.git)
 set(LIVRE_DEPENDS Qt4 REQUIRED Boost Collage Equalizer Lunchbox OpenGL PNG Threads)
 set(LIVRE_BOOST_COMPONENTS "date_time")
 set(LIVRE_QT4_COMPONENTS "QtCore QtGui")
+set(LIVRE_NOTEST ON)
 set(LIVRE_FORCE_BUILD ${CI_BUILD})
